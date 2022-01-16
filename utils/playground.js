56e8a6edd3039d51b0a82f55438746f7910170fa
paulpaul999 <82762352+paulpaul999@users.noreply.github.com>
2022-01-16 16:32:25 +0100

    introduce follow of http redirects

--- utils/playground.js
+++ utils/playground.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const { pipeline } = require('stream');
 
-const HTTPS_REJECT_UNAUTHORIZED = false;
+const MAX_REDIRECTS = 10;
 
 const filter_object = function (object, keys_whitelist) {
     const result = {};
@@ -71,16 +71,19 @@ const PASSTHROUGH_RES_HEADERS = [
  * @param {http.IncomingMessage} req - Node.js standard req object
  * @param {http.ServerResponse} res - Node.js standard res object
  * @param {String} url - URL to fetch from server
+ * @param {Object} options - 
  */
-const proxify_request = function (req, res, url) {
-    //req.url = dest_url
+const proxify_request = function (req, res, url, options, _recursion_level) {
+    console.log('SimpleProxy:', url);
+    if (_recursion_level === undefined) { _recursion_level = 0; }
+
     const url_parsed = new URL(url);
 
     const proxy_request_headers = filter_object(req.headers, PASSTHROUGH_REQ_HEADERS);
-    proxy_request_headers['user-agent'] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36";
+    if (options?.user_agent) { proxy_request_headers['user-agent'] = options.user_agent; }
 
     const proxy_request_options = {
-        rejectUnauthorized: HTTPS_REJECT_UNAUTHORIZED,
+        rejectUnauthorized: options?.https_reject_unauthorized || true,
         headers: proxy_request_headers,
     };
 
@@ -88,6 +91,18 @@ const proxify_request = function (req, res, url) {
     const proxy_request = handler.request(url_parsed, proxy_request_options, server_response => {
         console.log('statusCode:', server_response.statusCode);
         console.log('headers:', server_response.headers);
+
+        /* follow redirect */
+        if (server_response.headers.location) { /* NOTE: Also check for 301/302 code ? */
+            const max_redirects = options?.max_redirects === undefined ? MAX_REDIRECTS : options?.max_redirects;
+            const max_redirects_reached = _recursion_level >= max_redirects;
+            if (max_redirects_reached) {
+                return res.writeHead(502).end("Bad Gateway: Max redirects reached.");
+            }
+            const location = server_response.headers.location;
+            const next_url_parsed = new URL(location, url_parsed);
+            return proxify_request(req, res, next_url_parsed.toString(), options, _recursion_level+1);
+        }
 
         const res_code = server_response.statusCode;
         const res_headers = filter_object(server_response.headers, PASSTHROUGH_RES_HEADERS);
@@ -119,11 +134,21 @@ const proxify_request = function (req, res, url) {
 const server = http.createServer(function (req, res) {
     let url;
     url = "https://trailers.czechvr.com/czechvr/videos/download/468/468-czechvr-3d-7680x3840-60fps-oculusrift_uhq_h265-fullvideo-1.mp4";
+    url = "http://www.czechvr.com/category/1816-a-sweet-surprise-468-cvr/468-czechvr-big.jpg"; /** redirects with 301 to https full url */
     // url = "https://httpbin.org/gzip";
-    proxify_request(req, res, url);
+
+    const options = {
+        user_agent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.71 Safari/537.36",
+        https_reject_unauthorized: false,
+        max_redirects: 10,
+    };
+    proxify_request(req, res, url, options);
 });
   
 server.listen(3000);
+
+const myURL = new URL('http:/wiki.org/foo?asd=123&def72394#hallo', 'https://example.org/foo?asd=333');
+console.log('loggo', myURL.toString());
 
 // submit_to_proxy(1,2, "https://img2.badoink.com/content/scenes/325522/a-roll-in-the-hay-325522.jpg");
 // submit_to_proxy(1,2, "https://www.czechvr.com/category/1816-a-sweet-surprise-468-cvr/468-czechvr-big.jpg");
