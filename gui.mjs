ae5987c9cb727993e45ea87367dc08a91fd347a6
paulpaul999 <82762352+paulpaul999@users.noreply.github.com>
2022-02-11 01:52:58 +0100

    integrate preload script and basic lifecycle

--- gui.mjs
+++ gui.mjs
@@ -8,17 +8,50 @@ const require = createRequire(import.meta.url);
 
 const GUI = function () {
     const self = {};
-    const { BrowserWindow } = require('electron');
-    
-    const win = new BrowserWindow({ width: 800, height: 500 })
-    // win.loadURL('https://github.com')
-    win.loadFile(path.join(__dirname, 'public', 'electron', 'index.html'));
-    
-    const contents = win.webContents
-    // console.log(contents)
-    
-    // app.quit();
 
+    const { app, BrowserWindow, ipcMain, nativeTheme } = require('electron')
+    const path = require('path')
+
+    const createWindow = function () {
+        const win = new BrowserWindow({
+            width: 800,
+            height: 600,
+            webPreferences: {
+                preload: path.join(__dirname, 'public', 'electron', 'preload.cjs')
+            }
+        });
+
+        win.loadFile(path.join(__dirname, 'public', 'electron', 'index.html'));
+
+        ipcMain.handle('dark-mode:toggle', () => {
+            if (nativeTheme.shouldUseDarkColors) {
+                nativeTheme.themeSource = 'light'
+            } else {
+                nativeTheme.themeSource = 'dark'
+            }
+            return nativeTheme.shouldUseDarkColors
+        });
+
+        ipcMain.handle('dark-mode:system', () => {
+            nativeTheme.themeSource = 'system'
+        });
+    };
+
+    app.whenReady().then(() => {
+        createWindow()
+
+        app.on('activate', () => {
+            if (BrowserWindow.getAllWindows().length === 0) {
+                createWindow()
+            }
+        })
+    });
+
+    app.on('window-all-closed', () => {
+        if (process.platform !== 'darwin') {
+            app.quit()
+        }
+    });
 };
 
 export default GUI;
