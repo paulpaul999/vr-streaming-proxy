90eb5473e24cb0ff49190731217202ba78ee4004
paulpaul999 <82762352+paulpaul999@users.noreply.github.com>
2022-02-12 04:43:40 +0100

    simple toast msg

--- public/electron/script.js
+++ public/electron/script.js
@@ -1,8 +1,29 @@
 "use strict";
 
+const toast = async function (text_lines) {
+    // Get the snackbar DIV
+    const element = document.getElementById("toast");
+    element.textContent = '';
+
+    text_lines.forEach(line => {
+        const div = document.createElement('div');
+        div.textContent = line;
+        element.appendChild(div);
+    });
+
+    // Add the "show" class to DIV
+    element.classList.add("show");
+
+    // After 3 seconds, remove the show class from DIV
+    setTimeout(function () {
+        element.classList.remove("show"); 
+    }, 3000);
+};
+
 const listener_set_cookies = async function (event) {
     const state = await window.electron_api.provider_set_cookies(this.dataset.providerId);
     console.log(state);
+    toast(Object.entries(state).map((kv) => `${kv[0]}: ${kv[1]}`));
 };
 
 const main = async function () {
